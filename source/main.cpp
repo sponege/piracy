04e64f67f3634dddded26ebf9d880299bc2859d6
Jordan Perry <apples@jappl.es>
2024-01-11 23:55:18 -0700

    Piracy is No Party!

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 #include "hudson.h"
 #include "jail.h"
 #include "noparty.h"
+#include "poweroff.h"
 
 int main(void)
 {
@@ -21,32 +22,44 @@ int main(void)
 
 	int hudsonBg = bgInit(3, BgType_Bmp8, BgSize_B8_512x256, 8,0);
 	bgSetControlBits(hudsonBg, BG_WRAP_ON); // scrolling background
-	int piracyBg = bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
+	int nopartyBg = bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
 	int jailBg = bgInitSub(3, BgType_Bmp8, BgSize_B8_256x256, 0, 0);
+	int poweroffBg = bgInitSub(0, BgType_Text8bpp, BgSize_T_256x256, 30, 4);
 
 	dmaCopy(hudsonBitmap, bgGetGfxPtr(hudsonBg), 512*256);
 	dmaCopy(hudsonPal, BG_PALETTE, 256*2);
-	dmaCopy(nopartyTiles, bgGetGfxPtr(piracyBg), nopartyTilesLen);
-	dmaCopy(nopartyMap, bgGetMapPtr(piracyBg), nopartyMapLen);
+	dmaCopy(jailBitmap, bgGetGfxPtr(jailBg), 256*256);
+	dmaCopy(jailPal, BG_PALETTE_SUB, 256*2);
+
+	dmaCopy(nopartyTiles, bgGetGfxPtr(nopartyBg), nopartyTilesLen);
+	dmaCopy(nopartyMap, bgGetMapPtr(nopartyBg), nopartyMapLen);
+	dmaCopy(poweroffTiles, bgGetGfxPtr(poweroffBg), poweroffTilesLen);
+	dmaCopy(poweroffMap, bgGetMapPtr(poweroffBg), poweroffMapLen);
 	// you can only access extended palettes in LCD mode
   	vramSetBankE(VRAM_E_LCD); // for main engine
 	vramSetBankH(VRAM_H_LCD); // for sub engine
 	dmaCopy(nopartyPal,  &VRAM_E_EXT_PALETTE[0][0], nopartyPalLen);  // bg 0, slot 0
+	dmaCopy(poweroffPal,  &VRAM_H_EXT_PALETTE[0][0], poweroffPalLen);  // bg 0, slot 0
 	// map vram banks to extended palettes
 	// http://mtheall.com/banks.html#A=MBG0&C=MBG2&E=BGEPAL&H=SBGEPAL
 	vramSetBankE(VRAM_E_BG_EXT_PALETTE);     // for main engine
   	vramSetBankH(VRAM_H_SUB_BG_EXT_PALETTE); // for sub engine
-	dmaCopy(jailBitmap, bgGetGfxPtr(jailBg), 256*256);
-	dmaCopy(jailPal, BG_PALETTE_SUB, 256*2);
+
 
 	int x = 0;
 	int flipflop = 0; // flopflop slows scrolling by 2x
+	int frames = 0;
+	#define period 39
 	while(1) {
 		bgSetScroll(hudsonBg, x, x);
 		bgUpdate();
 		if(flipflop)x++;
 		x%=512; // lcm of 256 and 512 is 512, repeating pattern after scrolling 512 pixels
 		flipflop ^= 1;
+		frames++;
+		frames %= period * 2;
+		if (frames > period) bgHide(poweroffBg);
+		else bgShow(poweroffBg);
 		swiWaitForVBlank();
 	}
 
